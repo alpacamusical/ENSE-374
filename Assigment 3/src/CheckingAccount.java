89789c3fa2b779ed80aa18465bdd660ec24e5e20
Julio Silva <juansivas@outlook.com>
2019-10-18 22:50:44 -0600

    Final changes creation CheckingAccount and MoneyMarket classes end of assigment 3

---
+++ Assigment 3/src/CheckingAccount.java
@@ -0,0 +1,22 @@
+
+public class CheckingAccount {
+	private double checkingBalance;
+
+	public CheckingAccount(){
+	    
+	}
+	
+	public CheckingAccount(double cB){
+		checkingBalance = cB;
+	}
+	
+	public double getCheckingBalance() {
+		return checkingBalance;
+	}
+
+	public void setCheckingBalance(double checkingBalance) {
+		this.checkingBalance = checkingBalance;
+	}
+	
+	
+}
---
+++ Assigment 3/src/MoneyMarketAccount.java
@@ -0,0 +1,19 @@
+
+public class MoneyMarketAccount {
+    private double moneyMarketBalance;
+
+    public MoneyMarketAccount() {
+    	
+    }
+    public MoneyMarketAccount(double mMB) {
+    	moneyMarketBalance = mMB;
+    }
+	public double getMoneyMarketBalance() {
+		return moneyMarketBalance;
+	}
+
+	public void setMoneyMarketBalance(double moneyMarketBalance) {
+		this.moneyMarketBalance = moneyMarketBalance;
+	}
+    
+}
